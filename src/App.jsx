68c3a4946b5d831d66bcbb6272b0075f66053f84
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2024-08-02 12:38:52 -0300

    Finalizado o estudo consumindo api de filmes com react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Home } from "./pages/Home/index";
+
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <Home />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./global.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Details } from "./pages/Details";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<Details />} />
+    </Routes>
+  </BrowserRouter>
 );
